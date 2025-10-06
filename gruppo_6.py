5c98c24dbfc798b4d708645d2d03a2863d4b9b69
TommasoL24 <tommasolizzadri@gmail.com>
2025-10-06 15:59:23 +0200

    Punto due finito*
    *dimensione tubi da cambiare

--- gruppo_6.py
+++ gruppo_6.py
@@ -1,6 +1,8 @@
 import sys
 import Sofa
 import Sofa.Core
+import Sofa.Simulation
+import csv
 
 #definizione costanti
 Straight_length_1 = 115
@@ -16,6 +18,7 @@ Radius_curvature_1 = 105
 Radius_curvature_2 = 115
 Radius_curvature_3 = 135
 
+
 class KeyBoardController(Sofa.Core.Controller):
     def __init__(self, *args, **kwargs):
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
@@ -25,12 +28,6 @@ class KeyBoardController(Sofa.Core.Controller):
         self.rootNode = kwargs.get('rootNode')
         self.tube = 1    # ( 1 = TUBE_1, 2 = TUBE_2, 3 = TUBE_3 )
         self.action = 'k' 
-
-        #definizione del csv
-
-        #
-
-        #fine csv
 
         print("BENVENUTO")
         print("Selezionare il tubo da controllare (Ctrl + 1/2/3)")
@@ -61,32 +58,129 @@ class KeyBoardController(Sofa.Core.Controller):
         elif key == "M":
             print("Scelta rotazione oraria")
             self.action = 'm'
-            self.rotate(self.tube, -1.0)
+            self.rotate(self.tube, -0.4)
         elif key == "N":
             print("Scelta rotazione antioraria")
             self.action = 'n'
-            self.rotate(self.tube, 1.0)
+            self.rotate(self.tube, 0.4)
+        elif key == "W":
+            print("Generazione workspace")
+            self.generate_workspace("work_space_1.txt")
+
 
     #IMPLEMENTAZIONE CONTROLLO VERO E PROPRIO
 
     def translate(self, tube, quantity):
         with self.ir_controller.xtip.writeable() as d: d[tube-1] = d[tube-1] + quantity
     def rotate(self, tube, quantity):
-        with self.ir_controller.rotationInstrument.writeable() as d: d[tube-1] = d[tub-1]+ quantity
+        with self.ir_controller.rotationInstrument.writeable() as d: d[tube-1] = d[tube-1]+ quantity
 
+    def generate_workspace(self, filename):
+        workspace_points = []
 
+        insertion_range_1 = range(0, Straight_length_1 + Curved_length_1 + 3, 40)
+        rotation_range_1  = range(-90, 91, 45)
+        insertion_range_2 = range(0, Straight_length_2 + Curved_length_2 + 3, 40)
+        rotation_range_2  = range(-90, 91, 45)
+        insertion_range_3 = range(0, Straight_length_3 + Curved_length_3 + 3, 40)
+        rotation_range_3  = range(-90, 91, 45)
+
+        i=1
+
+        n_ins1 = len(insertion_range_1)
+        n_rot1 = len(rotation_range_1)
+        n_ins2 = len(insertion_range_2)
+        n_rot2 = len(rotation_range_2)
+        n_ins3 = len(insertion_range_3)
+        n_rot3 = len(rotation_range_3)
+
+        total_points = n_ins1 * n_rot1 * n_ins2 * n_rot2 * n_ins3 * n_rot3
+
+        for ins1 in insertion_range_1:
+            for rot1 in rotation_range_1:
+                for ins2 in insertion_range_2:
+                    for rot2 in rotation_range_2:
+                        for ins3 in insertion_range_3:
+                            for rot3 in rotation_range_3:
+
+                                print("Generazione punto",i,"/",total_points, end="\r")
+                            
+                                with self.ir_controller.xtip.writeable() as d:
+                                    d[0] = ins1
+                                    d[1] = ins2
+                                    d[2] = ins3
+                                with self.ir_controller.rotationInstrument.writeable() as d:
+                                    d[0] = rot1
+                                    d[1] = rot2
+                                    d[2] = rot3
+                                i=i+1
+                           
+                                Sofa.Simulation.animate(self.rootNode, self.rootNode.dt.value)
+
+                            
+                                tip_pose = self.rootNode.CTR.DOFs.position.value[-1]
+                                tip_position = tip_pose[:3]
+
+                            
+                                workspace_points.append([
+                                    ins1, rot1,
+                                    ins2, rot2,
+                                    ins3, rot3,
+                                    tip_position[0], tip_position[1], tip_position[2]
+                                ])
+
+    # salva su file
+        with open(filename, "w", newline="") as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow([
+                "Ins1", "Rot1", "Ins2", "Rot2", "Ins3", "Rot3", "X", "Y", "Z"
+            ])
+            writer.writerows(workspace_points)
+
+        print(f"Workspace salvato in {filename}")
 
 
 def createScene(rootNode):
 
     rootNode.addObject('RequiredPlugin', pluginName='Sofa.Component.AnimationLoop Sofa.Component.SolidMechanics.Spring MultiThreading Sofa.Component.Topology.Mapping Sofa.Component.Constraint.Lagrangian.Solver Sofa.Component.Constraint.Lagrangian.Correction Sofa.Component.Collision.Response.Contact Sofa.Component.Collision.Geometry Sofa.Component.Collision.Detection.Intersection Sofa.Component.Collision.Detection.Algorithm BeamAdapter Sofa.Component.Constraint.Projective Sofa.Component.LinearSolver.Direct Sofa.Component.ODESolver.Backward Sofa.Component.StateContainer Sofa.Component.Topology.Container.Constant Sofa.Component.Topology.Container.Grid Sofa.Component.Visual Sofa.Component.Mapping.Linear Sofa.Component.Topology.Container.Dynamic Sofa.GL.Component.Rendering3D')
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.IO.Mesh')  
     rootNode.addObject('VisualStyle', displayFlags='showVisualModels hideBehaviorModels showCollisionModels hideBoundingCollisionModels showForceFields')
     rootNode.addObject('FreeMotionAnimationLoop')
     rootNode.addObject('DefaultVisualManagerLoop')
     rootNode.gravity = [0, 0, 0]
     rootNode.dt = 0.05
 
-    #collision pipeline
+    # import mesh superficiale
+
+    surface_mesh = rootNode.addChild("SurfaceMesh")
+    surface_mesh.addObject('MeshOBJLoader', name='loader', filename="C:\SOFA_v25.06.00_Win64\plugins\ModelOrderReduction\share\sofa\examples\ModelOrderReduction\others\caduceus\mesh\snake_body.obj")
+
+    # Contenitore topologico (usa i dati del loader)
+    surface_mesh.addObject('MeshTopology', src='@loader')
+
+# Oggetto meccanico (posizioni nodi)
+    surface_mesh.addObject('MechanicalObject', name="dofs", template="Vec3d")
+
+# Modello grafico (OpenGL)
+    surface_mesh.addObject('OglModel', src='@loader', color=[0.8, 0.6, 0.2, 1.0], translation=[-25, 58, 0])
+
+# Mapping tra i dati meccanici e la grafica
+    #surface_mesh.addObject('IdentityMapping', input='@dofs', output='@visual')
+
+    #   -creazione mesh volumetrica
+
+   # volumetric_mesh = surface_mesh.addChild("VolumetricMesh")
+   # volumetric_mesh.addObject('Tetrahedralize', name='TetGen', method='Delaunay', elementSize=1.0)
+   # volumetric_mesh.addObject('MechanicalObject', name='dofs', template='Vec3d')
+   # volumetric_mesh.addObject('UniformMass', totalMass=1.0)
+   # volumetric_mesh.addObject('TetrahedronFEMForceField', name='FEM', youngModulus=1e5, poissonRatio=0.3)
+
+    #   -visualizzazione mesh
+
+    # volumetric_mesh.addObject('OglModel', color=[0.7,0.3,0.3,1.0])
+
+
+    # collision pipeline
 
     solver = rootNode.addObject('GenericConstraintSolver', name='solver', computeConstraintForces="1", tolerance='1e-6', maxIterations='1000')
     rootNode.addObject('CollisionPipeline', verbose='0')
@@ -99,13 +193,13 @@ def createScene(rootNode):
 
     TUBE_1 = rootNode.addChild('TUBE_1', bbox='-3 -6 -3 3 3 3')
 
-    #sezioni e unione
+    #   -sezioni e unione
 
     TUBE_1.addObject('RodStraightSection', name='StraightSection', length = Straight_length_1, radius = Tube_radius_1, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_1.addObject('RodSpireSection', name='SpireSection', length = Curved_length_1, spireDiameter = 2*Radius_curvature_1, spireHeight=0.0, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_1.addObject('WireRestShape', template='Rigid3d', name='RestShape_1', wireMaterials='@StraightSection @SpireSection')
 
-    #proprieta meccaniche
+    #   -proprieta meccaniche
 
     TUBE_1.addObject('EdgeSetTopologyContainer', name='meshLines_1')
     TUBE_1.addObject('EdgeSetTopologyModifier', name='Modifier')
@@ -116,13 +210,13 @@ def createScene(rootNode):
 
     TUBE_2 = rootNode.addChild('TUBE_2', bbox='-3 -6 -3 3 3 3')
 
-    #sezioni e unione
+    #   -sezioni e unione
 
     TUBE_2.addObject('RodStraightSection', name='StraightSection', length = Straight_length_2, radius = Tube_radius_2, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_2.addObject('RodSpireSection', name='SpireSection', length = Curved_length_2, spireDiameter = 2*Radius_curvature_2, spireHeight=0.0, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_2.addObject('WireRestShape', template='Rigid3d', name='RestShape_2', wireMaterials='@StraightSection @SpireSection')
 
-    #proprieta meccaniche
+    #   -proprieta meccaniche
 
     TUBE_2.addObject('EdgeSetTopologyContainer', name='meshLines_2')
     TUBE_2.addObject('EdgeSetTopologyModifier', name='Modifier')
@@ -133,20 +227,20 @@ def createScene(rootNode):
 
     TUBE_3 = rootNode.addChild('TUBE_3', bbox='-3 -6 -3 3 3 3')
 
-    #sezioni e unione
+    #   -sezioni e unione
 
     TUBE_3.addObject('RodStraightSection', name='StraightSection', length = Straight_length_3, radius = Tube_radius_3, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_3.addObject('RodSpireSection', name='SpireSection', length = Curved_length_3, spireDiameter = 2*Radius_curvature_3, spireHeight=0.0, youngModulus=1e9, massDensity=1.55e-6, nbBeams=40, nbEdgesCollis=40, nbEdgesVisu=80)
     TUBE_3.addObject('WireRestShape', template='Rigid3d', name='RestShape_3', wireMaterials='@StraightSection @SpireSection')
 
-    #proprieta meccaniche
+    #   -proprieta meccaniche
 
     TUBE_3.addObject('EdgeSetTopologyContainer', name='meshLines_3')
     TUBE_3.addObject('EdgeSetTopologyModifier', name='Modifier')
     TUBE_3.addObject('EdgeSetGeometryAlgorithms', name='GeomAlgo', template='Rigid3d')
     TUBE_3.addObject('MechanicalObject', template='Rigid3d', name='dofTopo_3')
 
-    #strumento complessivo
+    #STRUMENTO ASSEMBLATO
 
     CTR = rootNode.addChild('CTR')
     CTR.addObject('EulerImplicitSolver', rayleighStiffness='0.2', rayleighMass='0.1', printLog='false')
@@ -164,17 +258,17 @@ def createScene(rootNode):
     CTR.addObject('FixedProjectiveConstraint', name='FixedConstraint', indices='0')
     CTR.addObject('LinearSolverConstraintCorrection', wire_optimization='true')
 
-    #collisioni del CTR
+    #   -collisioni
 
     collis = CTR.addChild('Collis')
     collis.addObject('EdgeSetTopologyContainer', name='collisEdgeSet')
     collis.addObject('EdgeSetTopologyModifier', name='colliseEdgeModifier')
     collis.addObject('MechanicalObject', name='MechanicalObject', template='Vec3d')
-    #collis.addObject('MultiAdaptiveBeamMapping', controller='../IRController')
+    collis.addObject('MultiAdaptiveBeamMapping', controller='../IRController')
     collis.addObject('LineCollisionModel')
     collis.addObject('PointCollisionModel')
 
-    #proprieta visuali 1
+    #   -proprieta visuali 1
 
     visu_1 = CTR.addChild('visu_1', activated='true')
     visu_1.addObject('MechanicalObject', name='Quads')
@@ -187,7 +281,7 @@ def createScene(rootNode):
     visuOgl_1.addObject('OglModel', name='Visual', color='1 0 0 1', quads="@../Container_1.quads")
     visuOgl_1.addObject('IdentityMapping', input="@../Quads", output="@Visual")
 
-    #proprieta visuali 2
+    #   -proprieta visuali 2
 
     visu_2 = CTR.addChild('visu_2', activated='true')
     visu_2.addObject('MechanicalObject', name='Quads')
@@ -200,7 +294,7 @@ def createScene(rootNode):
     visuOgl_2.addObject('OglModel', name='Visual', color='0 1 0 1', quads="@../Container_2.quads")
     visuOgl_2.addObject('IdentityMapping', input="@../Quads", output="@Visual")
 
-    #proprieta visuali 3
+    #   -proprieta visuali 3
 
     visu_3 = CTR.addChild('visu_3', activated='true')
     visu_3.addObject('MechanicalObject', name='Quads')
@@ -217,10 +311,9 @@ def createScene(rootNode):
 
     rootNode.addObject(KeyBoardController(
         name="KeyBoardController",
-        irController=CTR.getObject("IRController"),
+        irController=CTR.getObject('IRController'),
         rootNode=rootNode
     ))
-
 
     return rootNode;
 
@@ -232,7 +325,7 @@ def main():
     root = Sofa.Core.Node('root')
     createScene(root)
     Sofa.Simulation.init(root)
-
+    Sofa.Gui.GUIManager.Init("volumetric_snake", "qglviewer")
     Sofa.Gui.GUIManager.Init('myscene', 'qglviewer')
     Sofa.Gui.GUIManager.createGUI(root, __file__)
     Sofa.Gui.GUIManager.SetDimension(1080, 1080)
